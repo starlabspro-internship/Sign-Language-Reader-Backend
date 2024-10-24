2904cd96e2555a6866b347042dd9b031350cd9cc
GENT HETEMI <GENTHETEMI7@GMAIL.COM>
2024-10-24 14:02:34 +0200

    Add Database

--- configuration.js
+++ configuration.js
@@ -1,3 +1,3 @@
 export const PORT = 5000; // default porti per html live server eshte 8000, nese ju duhet porti i back-ut mu ndrru mos e leni 8000
 
-export const DB_LINK = ''
+export const DB_LINK = 'mongodb+srv://genthetemi7:QGVSLmD0K2MRiVEx@sign-language-app.uydey.mongodb.net/?retryWrites=true&w=majority&appName=Sign-Language-App'
