01a4bef80c51f2aa8f64c600d99bbc98c1cbbaca
asaddsdasd <andimorina093@gmail.com>
2024-10-24 17:11:42 +0200

    CRUD API for Users

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 import express from 'express';
-import { PORT, DB_LINK } from './configuration.js';
 import mongoose from 'mongoose';
-
+import { PORT, DB_LINK } from './configuration.js';
+import userRoutes from './routes/user.routes.js';
 const app = express();
 
+// parse json body midelware
+app.use(express.json());
+
+// routat
+app.use('/api/users', userRoutes);
+
 mongoose.connect(DB_LINK)
-    .then(()=>{
-        console.log("App is connected to database")
-        app.listen(PORT, ()=>{
-            console.log(`server is running in port: ${PORT}`)
-        })
+    .then(() => {
+        console.log("App is connected to the database");
+        app.listen(PORT, () => {
+            console.log(`Server is running on port: ${PORT}`);
+        });
     })
-    
+    .catch((error) => {
+        console.error('Database connection failed:', error.message);
+    });
