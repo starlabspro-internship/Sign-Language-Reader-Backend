0c7c167abcad435cb3136f26312e9ca9c2d208d3
EndaAlidema <alidemaenda@gmail.com>
2024-10-25 05:54:11 +0200

    CRUD API for Questions

--- models/User.model.schema.js
+++ models/User.model.schema.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: { type: String, required: true, unique: true },
+  username: { type: String, required: [true, "Username is required!]"], unique: true },
   userpicture: { type: String },
   usersurname: { type: String },
-  useremail: { type: String, required: true, unique: true },
+  useremail: { type: String, required: true, unique: [true, "Email is required!"] },
   userphonenum: { type: String }, 
   userCompleted: [{
-    question_id: { type: Schema.Types.ObjectId, ref: 'Question', required: true },
+    question_id: { type: Schema.Types.ObjectId, ref: 'Question', required: [true," Question ID is required!"] },
     date_of_completion: { type: Date, default: Date.now }
   }]
 });
--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 import express from 'express';
 import { PORT, DB_LINK } from './configuration.js';
 import mongoose from 'mongoose';
+import questionRoutes from './routes/question.routes.js';  
+
 
 const app = express();
 
+
+app.use(express.json());
+
+
+app.use("/api/questions", questionRoutes);
+
+app.get('/', (req, res) => { res.send('Hello, the server is working!'); });
+
+
 mongoose.connect(DB_LINK)
-    .then(()=>{
-        console.log("App is connected to database")
-        app.listen(PORT, ()=>{
-            console.log(`server is running in port: ${PORT}`)
-        })
+    .then(() => {
+        console.log("App is connected to the database");
+        app.listen(PORT, () => {
+            console.log(`Server is running on port: ${PORT}`);
+        });
     })
-    
+    .catch((error) => {
+        console.error('Database connection failed:', error.message);
+    });
