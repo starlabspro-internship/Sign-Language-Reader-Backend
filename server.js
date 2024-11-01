af20039a03307393048b4f65bbc985f51da2026f
asaddsdasd <andimorina093@gmail.com>
2024-11-01 15:01:07 +0100

    Connection to front

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 import https from 'https';
 import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
-import cors from "cors";
+import cors from 'cors';
 import { PORT, DB_LINK } from './configuration.js';
 import userRoutes from './routes/user.routes.js';
-import questionRoutes from './routes/question.routes.js'; 
+import questionRoutes from './routes/question.routes.js';
 import signRoutes from './routes/sign.routes.js';
 import chapterRoutes from './routes/chapter.routes.js';
 import faqRoutes from './routes/faq.routes.js';
 
+// Get the current file path and directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
-//middlewares
+// Middlewares
 app.use(cookieParser());
 app.use(cors({
     origin: 'http://127.0.0.1:5500',
@@ -21,10 +27,12 @@ app.use(cors({
     credentials: true,
     allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
     exposedHeaders: ['Set-Cookie']
-  }));  
-
+}));
 app.use(express.json());
-app.use(express.static('production-front'));
+
+// Set the path for the frontend folder
+const frontendPath = path.join(__dirname, '../Sign-Language-Reader-Frontend');
+app.use(express.static(frontendPath));
 
 // Routes
 app.use('/api/users', userRoutes);
@@ -33,20 +41,24 @@ app.use('/api/chapters', chapterRoutes);
 app.use('/api/signs', signRoutes);
 app.use('/api/faq', faqRoutes);
 
+// Serve the home.html file on the base URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(frontendPath, 'home.html'));
+});
 
 mongoose.connect(DB_LINK)
-  .then(() => {
-    console.log("App is connected to the database");
+    .then(() => {
+        console.log("App is connected to the database");
 
-    const httpsOptions = {
-      key: fs.readFileSync('server.key'), 
-      cert: fs.readFileSync('server.cert') 
-    };
+        const httpsOptions = {
+            key: fs.readFileSync('server.key'),
+            cert: fs.readFileSync('server.cert')
+        };
 
-    https.createServer(httpsOptions, app).listen(PORT, () => {
-      console.log(`Secure server is running on port: ${PORT}`);
+        https.createServer(httpsOptions, app).listen(PORT, () => {
+            console.log(`Secure server is running on port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error('Database connection failed:', error.message);
     });
-  })
-  .catch((error) => {
-    console.error('Database connection failed:', error.message);
-  });
