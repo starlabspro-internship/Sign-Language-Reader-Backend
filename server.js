13bfba53cff827cf42f2f4301a51a5950d4fe89b
Enes Gj <enesgjurgjiali04@gmail.com>
2024-12-13 16:29:49 +0100

    add production URL

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 // Middlewares
 app.use(cookieParser());
 app.use(cors({
-    origin: ['https://localhost:8080', 'http://127.0.0.1:5500'],
+    origin: ['https://duarte.devops99.pro', 'https://localhost:8080','http://127.0.0.1:5500'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
     allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
